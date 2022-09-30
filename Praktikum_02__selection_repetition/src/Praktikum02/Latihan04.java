44c30b5d739c0042642ed002df9d2e40721827e6
RafiZuhair <21231019@student.ibik.ac.id>
2022-09-30 22:53:49 +0700

    praktikum 2

--- Praktikum_02__selection_repetition/src/Praktikum02/Latihan04.java
+++ Praktikum_02__selection_repetition/src/Praktikum02/Latihan04.java
@@ -1,67 +1,23 @@
 package Praktikum02;
 import java.util.Scanner;
 
-//Ferdy Apriliyanto - 212310012
-
 public class Latihan04 {
+
+	
 	public static void main(String[] args) {
-		int x;
-		String jwb;
+		int angka;
+		int i;
 		Scanner input = new Scanner(System.in);
+		System.out.println("masukkan angka = ");
+		angka = input.nextInt();
+				
 		
-		do {
-		System.out.println("DAFTAR BILANGAN");
-		System.out.println("1. FACTORIAL ASC");
-		System.out.println("2. FACTORIAL DESC");
-		System.out.print("Masukkan pilihan : ");
-		x = input.nextInt();
-		
-		switch (x) {
-		case 1:
-			faktorial(input);
-			break;
-		case 2:
-			faktorial_desc(input);
-			break;
-		
-			default:
-				System.out.println("salah");
-		}
-		
-		System.out.println("\nApa ingin mengulang (Y/N) ? = ");
-		jwb = input.nextLine();
-		}while (jwb.equals("Y"));
-		
-//		for(int a = 1; a <= x; a++) {
-//			String k = (a == x) ? "" : ",";
-//			System.out.print(a + k);
-//		}
-	
-	}
-	
-	static void faktorial(Scanner input) {
-		int sum = 1;
-		
-		System.out.print("Masukkan angka = ");
-		int f = input.nextInt();
-		
-		for (int i = 1; i <= f; i ++) {
-			sum = sum * i;
-			String k = (i == f ) ? " = " + sum: " x ";
-			System.out.print(i + k);
-		}
-	}
-	
-	static void faktorial_desc(Scanner input) {
-		int sum = 1;
-		
-		System.out.print("Masukkan angka = ");
-		int f = input.nextInt();
-		
-		for (int i = f; i >= 1; i--) {
-			sum = sum * i;
-			String k = (i == 1 ) ? " = " + sum: " x ";
-			System.out.print(i + k);
+				for (i = angka; i>0; i--) {
+					if (angka%i == 0) {
+						System.out.println("Hasil Faktorisasi dari "+angka+" =");
+						String koma = (i>1)?",":".";
+						String result += " "+i+koma;
+				}
 		}
 	}
 }
