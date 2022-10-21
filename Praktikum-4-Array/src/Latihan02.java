69e5be8069c75ee34e7607a55a9add9fe11c2c5e
RafiZuhair <21231019@student.ibik.ac.id>
2022-10-21 22:28:44 +0700

    update repo

---
+++ Praktikum-4-Array/src/Latihan02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Latihan02 {
+
+	public static void main(String[] args) {
+		int [] angka = new int[5];
+		
+		angka[0] = 1;
+		angka[1] = 1;
+		angka[2] = 1;
+		angka[3] = 1;
+		angka[4] = 1;
+		
+		
+		for (int i = 0; i <=4; i++) {
+			
+			System.out.println("number["+i+"]= " + angka[i]);
+		}
+	}
+
+}
